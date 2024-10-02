84c58950d4c3476afe860c8711eb5c790e93effd
Tymur Krasnopir <krasnopir.std@gmail.com>
2024-10-02 17:06:07 +0300

    Initial commit

---
+++ src/test/java/example/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package example.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
