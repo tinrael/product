da3414c487e4db9727f980688bda02a6c0a370a7
Tymur Krasnopir <krasnopir.std@gmail.com>
2024-10-03 20:45:31 +0300

    Add a Spring Boot Test for the GET endpoint

--- src/test/java/example/product/ProductApplicationTests.java
+++ src/test/java/example/product/ProductApplicationTests.java
@@ -1,11 +1,27 @@
 package example.product;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductApplicationTests {
-
+	@Autowired
+	TestRestTemplate testRestTemplate;
+	
+	@Test
+	void shouldReturnAProduct() {
+		ResponseEntity<String> responseEntity = testRestTemplate.getForEntity("/products/15", String.class);
+		
+		assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
+	
 	@Test
 	void contextLoads() {
 	}
