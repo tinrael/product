d4bc9e52712955b04f287ab4f675db48a5e1edd3
Tymur Krasnopir <krasnopir.std@gmail.com>
2024-10-03 22:08:41 +0300

    Update the test shouldReturnAProduct()

--- src/test/java/example/product/ProductApplicationTests.java
+++ src/test/java/example/product/ProductApplicationTests.java
@@ -10,6 +10,9 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductApplicationTests {
 	@Autowired
@@ -20,6 +23,13 @@ class ProductApplicationTests {
 		ResponseEntity<String> responseEntity = testRestTemplate.getForEntity("/products/15", String.class);
 		
 		assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+		
+		DocumentContext documentContext = JsonPath.parse(responseEntity.getBody());
+		Number id = documentContext.read("@.id");
+		String name = documentContext.read("@.name");
+		
+		assertThat(id).isEqualTo(15);
+		assertThat(name).isEqualTo("Pineapple");
 	}
 	
 	@Test
