a0034511bc87e25306e03e10cd9ff2e4d9d8b7e8
Tymur Krasnopir <krasnopir.std@gmail.com>
2024-10-04 17:03:00 +0300

    Update controller to return a Product

--- src/main/java/example/product/ProductController.java
+++ src/main/java/example/product/ProductController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/products")
 public class ProductController {
 	@GetMapping("/{id}")
-	private ResponseEntity<String> findById() {
-		return ResponseEntity.ok("{}");
+	private ResponseEntity<Product> findById() {
+		return ResponseEntity.ok(new Product(15L, "Pineapple"));
 	}
 }
