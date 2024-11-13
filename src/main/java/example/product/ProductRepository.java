a5ec82039853b76b87a6d0dcf93f760ad8007d30
Tymur Krasnopir <krasnopir.std@gmail.com>
2024-11-13 22:11:22 +0200

    Configure the ProductRepository

--- src/main/java/example/product/ProductRepository.java
+++ src/main/java/example/product/ProductRepository.java
@@ -2,5 +2,5 @@ package example.product;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductRepository extends CrudRepository {
+public interface ProductRepository extends CrudRepository<Product, Long> {
 }
