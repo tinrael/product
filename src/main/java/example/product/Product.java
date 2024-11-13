5d7e050333c66142a77fb0abca7615b9b8c47911
Tymur Krasnopir <krasnopir.std@gmail.com>
2024-11-13 22:12:29 +0200

    Configure the Product: add an Id annotation and implement Persistable<Long>

--- src/main/java/example/product/Product.java
+++ src/main/java/example/product/Product.java
@@ -1,5 +1,26 @@
 package example.product;
 
-record Product(Long id, String name) {
+import org.springframework.data.annotation.Id;
+import org.springframework.data.domain.Persistable;
 
+record Product(@Id Long id, String name) implements Persistable<Long> {
+    /**
+     * Returns the id of the entity.
+     *
+     * @return the id. Can be {@literal null}.
+     */
+    @Override
+    public Long getId() {
+        return this.id;
+    }
+
+    /**
+     * Returns if the {@code Persistable} is new or was persisted already.
+     *
+     * @return if {@literal true} the object is new.
+     */
+    @Override
+    public boolean isNew() {
+        return true;
+    }
 }
